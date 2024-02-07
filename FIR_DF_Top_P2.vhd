32d26e59ea49684b0e2c25fb85be528887d990d0
Hejazinezhad.SH <82455942+livewaves@users.noreply.github.com>
2024-02-07 13:10:55 +0100

    FIR_DF_Top_P2.vhd

--- FIR_DF_Top_P2.vhd
+++ FIR_DF_Top_P2.vhd
@@ -6,8 +6,8 @@ entity FIR_DF_Top is
 
     Port ( 
 				Clock 			: in  STD_LOGIC;
-				Input_Signal 	: in  signed (13 downto 0);
-				Output_Signal	: out	signed (13 downto 0)
+				Input_Signal 		: in  signed (13 downto 0);
+				Output_Signal		: out	signed (13 downto 0)
 				);
 				
 end FIR_DF_Top;
@@ -19,25 +19,25 @@ architecture Behavioral of FIR_DF_Top is
 	signal	Output_Signal_Int		:	signed (13 downto 0)		:=	(others=>'0');
 	
 	------	Delay line registers.
-	signal	Input_Signal_D1		:	signed (13 downto 0)		:=	(others=>'0');
+	signal	Input_Signal_D1			:	signed (13 downto 0)		:=	(others=>'0');
 	signal	Input_Signal_D11		:	signed (13 downto 0)		:=	(others=>'0');
-	signal	Input_Signal_D2		:	signed (13 downto 0)		:=	(others=>'0');
+	signal	Input_Signal_D2			:	signed (13 downto 0)		:=	(others=>'0');
 	signal	Input_Signal_D21		:	signed (13 downto 0)		:=	(others=>'0');
-	signal	Input_Signal_D3		:	signed (13 downto 0)		:=	(others=>'0');
+	signal	Input_Signal_D3			:	signed (13 downto 0)		:=	(others=>'0');
 	signal	Input_Signal_D31		:	signed (13 downto 0)		:=	(others=>'0');
 	
 	------	Coefficient constants.
-	Constant	Coeff_b0					:	signed (8 downto 0)		:=	to_signed(31,9);
-	Constant	Coeff_b1					:	signed (8 downto 0)		:=	to_signed(87,9);
-	Constant	Coeff_b2					:	signed (8 downto 0)		:=	to_signed(87,9);
-	Constant	Coeff_b3					:	signed (8 downto 0)		:=	to_signed(31,9);
+	Constant	Coeff_b0		:	signed (8 downto 0)		:=	to_signed(31,9);
+	Constant	Coeff_b1		:	signed (8 downto 0)		:=	to_signed(87,9);
+	Constant	Coeff_b2		:	signed (8 downto 0)		:=	to_signed(87,9);
+	Constant	Coeff_b3		:	signed (8 downto 0)		:=	to_signed(31,9);
 	
 	signal	Product_Reg0			:	signed (22 downto 0)		:=	(others=>'0');
 	signal	Product_Reg1			:	signed (22 downto 0)		:=	(others=>'0');
 	signal	Product_Reg2			:	signed (22 downto 0)		:=	(others=>'0');
 	signal	Product_Reg3			:	signed (22 downto 0)		:=	(others=>'0');
 	
-	signal	FIR_Accumulator		:	signed (22 downto 0)		:=	(others=>'0');
+	signal	FIR_Accumulator			:	signed (22 downto 0)		:=	(others=>'0');
 	
 begin
 
@@ -49,14 +49,14 @@ begin
 		if rising_edge(Clock) then
 		
 			-- Input signal buffering
-			Input_Signal_Int			<=	Input_Signal;
+			Input_Signal_Int		<=	Input_Signal;
 			
 			-- FIR delay line
-			Input_Signal_D1			<=	Input_Signal_Int;
+			Input_Signal_D1				<=	Input_Signal_Int;
 			Input_Signal_D11			<=	Input_Signal_D1;
-			Input_Signal_D2			<=	Input_Signal_D11;
+			Input_Signal_D2				<=	Input_Signal_D11;
 			Input_Signal_D21			<=	Input_Signal_D2;
-			Input_Signal_D3			<=	Input_Signal_D21;
+			Input_Signal_D3				<=	Input_Signal_D21;
 			Input_Signal_D31			<=	Input_Signal_D3;
 			
 			-- Product register
@@ -66,7 +66,7 @@ begin
 			Product_Reg3				<=	Input_Signal_D31 * Coeff_b3 + Product_Reg2;
 			
 			-- FIR multiply and accumulate
-			FIR_Accumulator			<=	Product_Reg3;
+			FIR_Accumulator				<=	Product_Reg3;
 			
 			Output_Signal_Int			<=	FIR_Accumulator(21 downto 8);
 						
